22f6058cb0ebf4d9014277944032053f4262c3c4
KadePham <kadepham369@gmail.com>
2025-05-05 10:48:49 +0700

    Initilize structure of FightingGame2D

---
+++ src/com/fightinggame/input/InputHandler.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fightinggame.input;
+
+/**
+ *
+ * @author OceanShark
+ */
+public class InputHandler {
+    
+}
